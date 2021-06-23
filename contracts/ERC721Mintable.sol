09e907b25323388bcf006c0b7a4267d672e3d153
Nikolai Golub <nikolay.v.golub@gmail.com>
2021-06-23 22:54:51 +0200

    Set tokenUri

--- contracts/ERC721Mintable.sol
+++ contracts/ERC721Mintable.sol
@@ -568,14 +568,10 @@ contract ERC721Metadata is ERC721Enumerable, usingOraclize {
         _setTokenURI(tokenId);
     }
 
-    // TODO: Create an internal function to set the tokenURI of a specified tokenId
-    // It should be the _baseTokenURI + the tokenId in string form
-    // TIP #1: use strConcat() from the imported oraclizeAPI lib to set the complete token URI
-    // TIP #2: you can also use uint2str() to convert a uint to a string
-    // see https://github.com/oraclize/ethereum-api/blob/master/oraclizeAPI_0.5.sol for strConcat()
-    // require the token exists before setting
     function _setTokenURI(uint256 tokenId) internal {
         require(_exists(tokenId));
+        string memory tokenUri = usingOraclize.strConcat(_baseTokenURI, usingOraclize.uint2str(tokenId));
+        _tokenURIs[tokenId] = tokenUri;
     }
 }
 
