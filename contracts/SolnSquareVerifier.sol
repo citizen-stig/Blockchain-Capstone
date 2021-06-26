bf4d0632a7610a2ea11703248bda5d2483da2cb4
Nikolai Golub <nikolay.v.golub@gmail.com>
2021-06-26 19:52:34 +0200

    Export Verifier from ZoKrates

--- contracts/SolnSquareVerifier.sol
+++ contracts/SolnSquareVerifier.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >= 0.8.4;
 
+import "./Verifier.sol";
+
 // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
 contract SquareVerifier {
 
